6f66537db7865d3d8bafe6a64ac3d2aca122fd63
shahrizoda <shahrizodadeveloping@gmail.com>
2022-04-11 14:48:59 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -125,5 +125,23 @@ new Swiper(".systems__swiper", {
     },
 });
 
+$(document).ready(function() {
+    $(".accordion > .accordion__button").on("click", function() {
+      if ($(this).hasClass("active")) {
+        $(this).removeClass("active");
+        $(this)
+          .siblings(".accordion__content")
+          .slideUp(200);
+      } else {
+        $(".accordion > .accordion__button").removeClass("active");
+        $(this).addClass("active");
+        $(".accordion__content").slideUp(200);
+        $(this)
+          .siblings(".accordion__content")
+          .slideDown(200);
+      }
+    });
+  });   
+
 
 
